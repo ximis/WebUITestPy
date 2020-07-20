67e4cba80819270481dffd12ee8bf685d18eac9a
pinrong <pinrong@pinrongdeMacBook-Pro.local>
2020-07-20 10:57:08 +0800

    add

--- testcases/test_login_page.py
+++ testcases/test_login_page.py
@@ -10,7 +10,7 @@ class TestLogin:
     def test_login_success(self):
         main = main_page.MainPage()
         login_page = main.to_login_page()
-        login_page.login("test++2@kucoin.com", "a123456")
+        login_page.login("username", "password")
 
 
 
