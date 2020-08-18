8401b092c0761b3e767c1075db08b9907fb070e7
pinrong <pinrong@pinrongdeMacBook-Pro.local>
2020-08-18 15:41:32 +0800

    add allure attachment test and conftest workaroud test

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,6 +1,9 @@
 from common import config_load
 from pages.main_page import MainPage
 import pytest
+import time
+from datetime import datetime
+import allure
 
 
 def pytest_addoption(parser):
@@ -22,3 +25,40 @@ def init(request):
     config_load.init(request.config.option.env)
     if request.config.option.headless is not None:
         config_load.set_options("headless", True)
+
+
+# set up a hook to be able to check if a test has failed
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
+# check if a test has failed
+@pytest.fixture(scope="function", autouse=True)
+def test_failed_check(request):
+    yield
+    # request.node is an "item" because we use the default
+    # "function" scope
+    if request.node.rep_setup.failed:
+        print("setting up a test failed!", request.node.nodeid)
+    elif request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            # find a way to get driver
+            driver = request.node.funcargs['page'].driver
+            take_screenshot(driver, request.node.nodeid)
+            print("executing test failed", request.node.nodeid)
+
+
+# make a screenshot with a name of the test, date and time
+def take_screenshot(driver, nodeid):
+    time.sleep(1)
+    file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.png'.replace("/", "_").replace("::", "__")
+    driver.save_screenshot(file_name)
+    allure.attach.file(file_name, attachment_type=allure.attachment_type.PNG)
---
+++ testcases/test_baidu_search.py
@@ -0,0 +1,24 @@
+import allure
+import pytest
+from pages import baidu_search_page
+
+
+@allure.feature("搜索")
+class TestBaiduSearch:
+
+    @pytest.fixture(scope="class")
+    def page(self):
+        page = baidu_search_page.BaiduSearchPage()
+        yield page
+        page.quit()
+
+    @allure.story("search and check url")
+    def test_search(self, page):
+        page.search("selenium")
+        assert "selenium" in page.get_url()
+
+    @allure.story("search 2")
+    def test_search2(self, page):
+        page.search("selenium")
+        assert "selenium" not in page.get_url()
+
